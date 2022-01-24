6d0a661f9f9cfd86c4f946840743b7035ba40ed2
ManirambonaAlainVeron <avmanirambona@gmail.com>
2022-01-24 21:56:52 +0200

    first commit

---
+++ src/main/java/com/example/try_spring_seriously/TrySpringSeriouslyApplication.java
@@ -0,0 +1,13 @@
+package com.example.try_spring_seriously;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrySpringSeriouslyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrySpringSeriouslyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/try_spring_seriously/TrySpringSeriouslyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.try_spring_seriously;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrySpringSeriouslyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
